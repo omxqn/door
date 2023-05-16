6ef4a5e0dc9000cec3c5249d58998d4d4ddcd666
Azzam <48726420+omxqn@users.noreply.github.com>
2023-05-16 17:36:09 +0400

    Add files via upload

--- dd.py
+++ dd.py
@@ -4,15 +4,42 @@ import evdev
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 rfid_device = None
 for device in devices:
-    if device.name == 'RFID reader':
+    print(device)
+    if device.name == 'BARCODE SCANNER Keyboard Interface':
         rfid_device = device
         break
+data = []
 
-# If the RFID reader was found, read from it
-if rfid_device is not None:
-    print('Found RFID reader:', rfid_device.name)
-    for event in rfid_device.read_loop():
-        if event.type == evdev.ecodes.EV_KEY and event.value == 1:
-            print('RFID tag scanned:', evdev.categorize(event).keycode)
-else:
-    print('RFID reader not found.')
+def read():
+    # If the RFID reader was found, read from it
+    if rfid_device is not None:
+        if data == []:
+            print('Found RFID reader:', rfid_device.name)
+        global rfid_tag
+        rfid_tag = ''
+        for event in rfid_device.read_loop():
+            if event.type == evdev.ecodes.EV_KEY and event.value == 1:
+                digit = evdev.categorize(event).keycode
+                if digit == "KEY_ENTER" :
+                    if rfid_tag in data:
+                        print(f"User with id: {rfid_tag} is in database and Enterd the gate")
+                        break
+                    else:
+                        id = rfid_tag
+                        rfid_tag = ""
+                        print(f'RFID tag ({id}) has been added:')
+                        data.append(id)
+
+                        break
+	    	
+                else:
+
+                   
+                    if digit[-1].isdigit():
+                        rfid_tag+= digit[-1]
+    else:
+        print('RFID reader not found.')
+
+        
+while True:
+    read()
